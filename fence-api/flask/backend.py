7cd94eef9d1b2a0f17f6010d0db33085655de731
Arani Bhattacharyay <aranibatta@berkeley.edu>
2015-01-17 19:10:50 -0500

    post/get

--- fence-api/flask/backend.py
+++ fence-api/flask/backend.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify, abort, url_for, request, make_response
 from pymongo import MongoClient
 import math
 
@@ -53,13 +54,14 @@ def update_loc(user_id, new_loc):
 		# DECREMENTED users.find({"_id": user_id}).prey_id = None
 
 # finds users within 1/2 mile get
+@app.route('/backend/getNearby/<>', methods=['GET'])
 def getNearby(user_id):
 	cur_user = users.find_one({"_id": user_id})
 	# Radius of about 1/2 mile
 	return db.users.find({"loc": {"$within": {"$center": [cur_user["loc"], float(1)/138]}}})
 	# DECREMENTED return users.find({"loc": SON([("$near", cur_user["loc"]), ("$maxDistance", 1/138)])})
 
-# called when user kills his/her target
+# called when user kills his/her target POST
 def killed(user_id):
 	users.update({"_id":user_id}, {"$set": {"hunt_id":None}}, upsert=False)
 	cur_user = users.find_one({"_id": user_id})
@@ -67,12 +69,14 @@ def killed(user_id):
 
 # determines whether the user assigned to hunt you is still hunting you
 # Decremented get
+@app.route('/backend/whetherStillHunted', methods=['GET'])
 def hunted(prey_id):
 	if users.find_one({"_id":prey_id}) != None and users.find_one({"_id":prey_id})["hunt_id"] == None:
 		return False
 	return True
 
 # determine if the players are too far apart get
+@app.route('/backend/too_far', methods=['GET'])
 def too_far(id_1, id_2):
 	user1 = users.find_one({"_id": id_1})
 	user2 = users.find_one({"_id": id_2})
